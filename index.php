e0e4098d5e20cca55c1a4f2d7be245c96f54e4e6
radprad <47772339+radprad@users.noreply.github.com>
2023-06-28 22:34:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 	<h1>MySQL Table Viewer</h1>
 	<?php
 		// Define database connection variables
-		$servername = "DBServer";
-		$username = "DB_USER";
-		$password = "DB_PASSWORD";
-		$dbname = "DB_NAME";
+		$servername = "gl-mysql-server.mysql.database.azure.com";
+		$username = "glsqladmin";
+		$password = "**Gladministrator";
+		$dbname = "mydatabase";
 
 		// Create database connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
